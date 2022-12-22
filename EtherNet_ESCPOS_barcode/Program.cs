8590374d78254c2d3ba0ed6ddf66868fafada5de
LeviyLi <lifeifei@nti56.com>
2022-12-22 17:37:17 +0800

    test1002

--- EtherNet_ESCPOS_barcode/Program.cs
+++ EtherNet_ESCPOS_barcode/Program.cs
@@ -8,7 +8,7 @@ namespace Serial_ESCPOS
     static class Program
     {
         /// <summary>
-        /// 应用程序的主入口点。
+        /// 应用程序的主入口点。123test
         /// </summary>
         [STAThread]
         static void Main()
